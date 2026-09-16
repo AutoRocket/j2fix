0840e1ee2af51e96c307e96e31b2113db34181fd
charles.durber <charles.durber@opticoreit.com>
2026-09-16 14:53:35 +0100

    update version

--- src/j2fix/__init__.py
+++ src/j2fix/__init__.py
@@ -4,4 +4,4 @@ from .formatter import FormatOptions, format_text
 from .limits import TemplateLimitError
 
 __all__ = ["FormatOptions", "TemplateLimitError", "format_text"]
-__version__ = "0.2.0"
+__version__ = "0.3.0"
